6c8c23e555516e9bb5bcbcaaec10211d622d9e1c
Krishna Kant Pathak <krishnakantp002@gmail.com>
2026-02-03 01:50:57 +0530

    course service implemented successfully

--- src/main.js
+++ src/main.js
@@ -2,6 +2,7 @@ const express = require('express')
 const dotenv = require ('./config/db.js')
 const userRouter = require('./routes/user.routes.js')
 const createUserTable = require('./data/createUsertable.js')
+const createCourseTable = require('./data/createCourseTable.js')
 const app = express()
 
 app.use(express.json())
@@ -10,6 +11,7 @@ app.use(express.urlencoded({extended:true}))
 app.use('/api',userRouter)
 
 createUserTable()
+createCourseTable()
 
 PORT = process.env.PORT || 3000;
 app.listen(PORT,()=>{
